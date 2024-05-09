f68b6a297c57b5f6eb725470eef207748c788a4f
kunxin-chor <chorkunxin@yahoo.com>
2024-05-09 08:07:35 +0000

    Test being able to get data from table with JPA

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,15 +2,28 @@ package com.example.demo;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.Product;
+import com.example.demo.services.ProductService;
 
 
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private ProductService productServices;
+
     @GetMapping("/")
     public Map<String, String> hello(){
         return Map.of("message", "hello world again");
+    }
+
+    @GetMapping("/products")
+    public Iterable<Product> products() {
+        return productServices.getAllProducts();
     }
 }
